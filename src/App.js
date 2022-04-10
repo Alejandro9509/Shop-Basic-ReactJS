c97905e6e38047609182e9bdd1c2a7fdfefda5dd
Alejandro9509 <alejandrobringas2021@gmail.com>
2022-04-10 01:19:00 -0700

    Adding mapping to props

--- src/App.js
+++ src/App.js
@@ -2,12 +2,63 @@ import './App.scss'
 import { BrowserRouter as Router, 
          Routes,
          Route, 
-         NavLink, 
-         Link
+         NavLink
         } from "react-router-dom"
 import { Home } from './pages/home'
 import { About } from './pages/about'
 import { NotFound } from './pages/NotFound'
+import { createStore } from 'redux'
+
+const ADD = "ADD"
+const REMOVE = "REMOVE"
+
+const Products = [
+
+]
+//Redux
+const messageReducer = (p = [], action) => {
+    switch (action.type) {
+        case ADD: 
+            Products.push(action.product)
+        return  Products
+        case REMOVE: 
+        return Products
+        default: 
+        return p
+    }
+}
+
+const addProduct = (product) => {
+    return(
+        {
+            type: ADD, 
+            product
+        }, 
+        {
+            type: REMOVE, 
+            product
+        }
+    )
+}
+const store = createStore(messageReducer)
+//React
+
+const  mapStateToProps = (state) => {
+  return {
+    products: state
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return  {
+    submitNewProduct :(prod) => {
+      dispatch(addProduct(prod))
+    } 
+  }
+}
+
+
+
 function App() {
   return (
     <Router>
