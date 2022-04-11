e4661e14316fbd92393b6bbdd53fb61295f70753
Alejandro9509 <alejandrobringas2021@gmail.com>
2022-04-10 18:26:38 -0700

    Adding add producto button

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,54 @@
 
 import { Provider, connect } from 'react-redux'
-import React from 'react'
-import {Card, ListGroup } from 'react-bootstrap'
+import React, { useState } from 'react'
+import {Card, ListGroup, Modal , Button} from 'react-bootstrap'
 import {store, mapDispatchToProps, mapStateToProps} from '../redux/productRedux'
+
+function ModalView() {
+    const [show, setShow] = useState(false);
+  
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+  
+    return (
+      <>
+        <Button variant="primary" onClick={handleShow}>
+          Añadir producto
+        </Button>
+  
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    );
+  }
+  
 
 class AppProcessing extends React.Component {
     constructor(props) {
         super(props)
-        this.subProd = this.subProd.bind(this)       
+        this.subProd = this.subProd.bind(this)
+
+        
+    }
+    
+    handleClose = () => {
+        this.setShow(false)
+    }
+
+    handleShow = () => {
+        this.setShow(true)
     }
     
     subProd() {
@@ -15,12 +56,16 @@ class AppProcessing extends React.Component {
     }
     render() {
         return(
+            <>
             <section>
                 <div>
-                    <button onClick={this.subProd}>Presioname</button>
+                    <ModalView />
                     <div>{this.props.products}</div>
                 </div>
             </section>
+
+           
+            </>
         )
     }
 }
