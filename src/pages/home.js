ee9c50681df9d7d8c70b0976970626abcac3bd07
Alejandro9509 <alejandrobringas2021@gmail.com>
2022-04-12 01:52:33 -0700

    Prevente to add repeat productos

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,6 @@
 
 import { Provider, connect } from 'react-redux'
-import React, { useState, useEffect } from 'react'
+import React, { useState} from 'react'
 import { Modal , Button, InputGroup,  FormControl, Table} from 'react-bootstrap'
 import {store, mapDispatchToProps, mapStateToProps} from '../redux/productRedux'
 
@@ -10,22 +10,27 @@ function ModalView(props) {
 
     const [show, setShow] = useState(false);
     const [prod, setProduct] = useState("");
-    const [price, setPrice] = useState(0); 
-    const divField = React.useRef(null); 
+    const [price, setPrice] = useState(0);
+    const divPrecio = React.useRef(null);
+    const divProducto = React.useRef(null);
 
- 
+
     const handleProd = (event) => {
         setProduct(event.target.value)
-    } ; 
+    } ;
     const handlePrice = (event) => {
         setPrice(event.target.value)
     }
     const handleSubmit = () => {
-        console.log()
-        if(Number(price)) {
-                        props.submit.submitNewProduct([prod, price]);
-                        setShow(false); 
-                        }else divField.current.className = 'alert alert-danger'
+        if(props.submit.products.filter((item) => item[0] == prod).length > 0){
+          divProducto.current.className = 'alert alert-danger'
+        }else{
+          if(Number(price)) {
+                          props.submit.submitNewProduct([prod, price]);
+                          setShow(false);
+                        }else divPrecio.current.className = 'alert alert-danger'
+        }
+
     }
     return (
       <div>
@@ -37,10 +42,11 @@ function ModalView(props) {
             <Modal.Title>Añadir producto</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-          <InputGroup className="mb-3">        
+          <div ref={divProducto} className='d-none'>Ya se encuentra este producto</div>
+          <InputGroup className="mb-3">
             <FormControl onChange={ handleProd.bind(this) } id="product-name" aria-describedby="basic-addon3" />
           </InputGroup>
-          <div ref={divField} className='d-none'>Verifica que sea un valor numerico</div>
+          <div ref={divPrecio} className='d-none'>Verifica que sea un valor numerico</div>
           <InputGroup className="mb-3">
             <InputGroup.Text>$</InputGroup.Text>
             <FormControl onChange={ handlePrice.bind(this) } id="product-price" aria-label="Amount (to the nearest dollar)" />
@@ -58,7 +64,7 @@ function ModalView(props) {
         </Modal>
       </div>
     );
-  }
+}
 
 function ModalViewCobrar(props){
   const [cobrar, setCobrar] = useState(false)
@@ -83,19 +89,13 @@ function ModalViewCobrar(props){
   )
 }
 
-
-
 class AppProcessing extends React.Component {
     constructor(props) {
         super(props)
         this.handleDel = this.handleDelete.bind(this)
     }
-
-
-
     removeProd(index){
       console.log(index)
-      
     }
     handleDelete = (event) => {
       this.props.removeAProduct(event.target.id)
@@ -104,7 +104,7 @@ class AppProcessing extends React.Component {
         return(
             <>
             <section>
-                <div className='box-principal' > 
+                <div className='box-principal' >
                     <div>
                         <ModalView submit={this.props} />
                     </div>
@@ -154,7 +154,4 @@ export class Home extends React.Component {
       </Provider>
     )
   }
-} 
-
-
-
+}
